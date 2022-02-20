991d1685dae917e7d6bac4415c87314982d99e68
Yaroslav4200 <yarik.ilin@gmail.com>
2022-02-20 14:05:19 +0300

    Add files via upload

---
+++ Практическое задание 9/Задание 2.py
@@ -0,0 +1,17 @@
+# Вариант 7. Заданы N строк слов, которые вводятся с клавиатуры. Вводится слог (последовательность букв). Подсчитать количество слогов в каждой строке
+import re
+
+N = 2
+strings = []
+
+for i in range(0, N):
+    print('Введите строку:', end=' ')
+    strings.append(input())
+
+print('Введите слог:', end=' ')
+a = input()
+
+for string in strings:
+    count = len(re.findall(a, string))
+    print('Слог \'%s\' встречается в 1-ой строке %s раз' % (a, count))
+    print('Слог \'%s\' встречается во 2-ой строке %s раз' % (a, count))
