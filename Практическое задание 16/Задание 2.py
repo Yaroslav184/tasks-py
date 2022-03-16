e00b8cf274c86dcb075b9f7faedf458180dbc1bd
Yaroslav184 <yarik.ilin@gmail.com>
2022-03-16 16:41:27 +0300

    Update Задание 2.py

--- Практическое задание 16/Задание 2.py
+++ Практическое задание 16/Задание 2.py
@@ -1,50 +1,39 @@
-class Country:
-    def __init__(self, country='Неизвестно', data=1, president='Кто-то', square=1):
+class Country_1:
+    def __init__(self, country='Неизвестно', data='Неизвестно', president='Кто-то', square='Неизвестно'):
         self.country = country
         self.data = data
         self.president = president
         self.square = square
-        print('Создан!')
-        print('-----------------------')
 
-self1 = Country('Россия', 1997, 'Владимир Путин', 17098246)
+    def __str__(self):
+        a = 'Название страны: ' + str(self.country) + ' Дата основания: ' + str(self.data) + ' Президент: ' + str(self.president) + ' Площадь: ' + str(self.square)
+        return a
 
-def countryInfo():
-    print('Название страны:', self1.country)
-    print('Дата основания:', self1.data)
-    print('Президент:', self1.president)
-    print('Площадь:', self1.square)
+country_1 = Country_1('Россия', '1997', 'Владимир Путин', '17098246')
+country_2 = Country_1('Латвия', '1918', 'Эгилс Левитс', '64589')
+country_3 = Country_1('Беларусь', '1919', 'Александр Лукашенко', '207600')
+print(country_1)
+print(country_2)
+print(country_3)
+print('-------------------------')
 
-countryInfo()
+class Country_2:
+    def __init__(self, country='Неизвестно', data='Неизвестно', president='Кто-то', square='Неизвестно'):
+        self.country = country
+        self.data = data
+        self.president = president
+        self.square = square
 
-def __init__(self, country='Неизвестно', data=1, president='Кто-то', square=1):
-    self.country = country
-    self.data = data
-    self.president = president
-    self.square = square
+    def __repr__(self):
+        return f'({self.country}, {self.data}, {self.president}, {self.square})'
 
-self2 = Country('Латвия', 1918, 'Эгилс Левитс', 64589)
-
-def countryInfo():
-    print('Название страны:', self2.country)
-    print('Дата основания:', self2.data)
-    print('Президент:', self2.president)
-    print('Площадь:', self2.square)
-
-countryInfo()
-
-def __init__(self, country='Неизвестно', data=1, president='Кто-то', square=1):
-    self.country = country
-    self.data = data
-    self.president = president
-    self.square = square
-
-self3 = Country('Беларусь', 1919, 'Александр Лукашенко', 207600)
-
-def countryInfo():
-    print('Название страны:', self3.country)
-    print('Дата основания:', self3.data)
-    print('Президент:', self3.president)
-    print('Площадь:', self3.square)
-
-countryInfo()
+country_1 = Country_1('Россия', '1997', 'Владимир Путин', '17098246')
+country_2 = Country_1('Латвия', '1918', 'Эгилс Левитс', '64589')
+country_3 = Country_1('Беларусь', '1919', 'Александр Лукашенко', '207600')
+print(country_1)
+print(country_2)
+print(country_3)
+print('-------------------------')
+b = [country_1, country_2, country_3]
+print(b)
+print(type(b))
